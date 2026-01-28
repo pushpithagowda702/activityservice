4adffbb4808d40d0efcc3c81eb288b0405f2d795
Pushpitha <pushpithagowda776@gmail.com>
2026-01-29 00:01:30 +0530

    gateway server updated

--- src/main/java/com/fitness/activityService/controller/ActivityController.java
+++ src/main/java/com/fitness/activityService/controller/ActivityController.java
@@ -17,12 +17,15 @@ public class ActivityController {
     private ActivityService activityService;
 
     @PostMapping("/create-activity")
-    public ResponseEntity<ActivityResponse> trackActivity(@RequestBody ActivityRequest request) {
+    public ResponseEntity<ActivityResponse> trackActivity(@RequestBody ActivityRequest request, @RequestHeader("X-User-Id") String userId) {
+        if(userId != null) {
+            request.setUserId(userId);
+        }
         return ResponseEntity.ok(activityService.trackActivity(request));
     }
 
     @GetMapping
-    public ResponseEntity<List<ActivityResponse>> getUserActivities(@RequestHeader("X-User-ID") String userId) {
+    public ResponseEntity<List<ActivityResponse>> getUserActivities(@RequestHeader("X-User-Id") String userId) {
         return ResponseEntity.ok(activityService.getUserActivities(userId));
     }
 }
